3735d15345879e7b5cd3a1f692025a6037bacb13
RossCr0ss <sobechko.r@gmail.com>
2022-05-18 23:59:59 +0300

    Create independent component Expense Date.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const weekday = props.date.toLocaleDateString('en-US', { weekday: 'long' });
-  const month = props.date.toLocaleDateString('en-US', { month: 'long' });
-  const year = props.date.toLocaleDateString('en-US', { year: 'numeric' });
-  const day = props.date.toLocaleDateString('en-US', { day: 'numeric' });
   return (
     <div className="expense-item">
-      <div className="expense-item__date">
-        <span className="weekday">{weekday}</span>
-        <span className="day">{day}</span>
-        <p>
-          <span className="month">{month}</span>
-          <span className="year">{year}</span>
-        </p>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__desc">
         <p>{props.title}</p>
         <h4>$ {props.amount}</h4>
