614b742152445b56b57c16123f538e3f834ad6e3
RossCr0ss <sobechko.r@gmail.com>
2022-05-22 10:50:33 +0300

    Added changes to Expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const changeTitleHandler = () => {
+    setTitle('Updated!');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__desc">
-        <p>{props.title}</p>
+        <p>{title}</p>
         <h4>$ {props.amount}</h4>
       </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
   );
 };
