e5d8639d9ecd168488dcb90ebe6bf4224ba46c6d
RossCr0ss <sobechko.r@gmail.com>
2022-05-22 12:22:06 +0300

    Add state ti expense from

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,55 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [formValues, setFormValues] = useState({
+    title: '',
+    price: '',
+    date: '',
+  });
+  const changeTitleHandler = (event) => {
+    setFormValues({ ...formValues, title: event.target.value });
+  };
+
+  const changeAmountHandler = (event) => {
+    setFormValues({ ...formValues, price: event.target.value });
+  };
+
+  const changeDateHandler = (event) => {
+    setFormValues({ ...formValues, date: event.target.value });
+  };
   return (
     <form className="expense-form">
       <div className="controls">
         <div className="controls__item">
-          <label for="name">Text</label>
-          <input type="text" id="name" />
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={changeTitleHandler}
+            name="title"
+            placeholder="Enter name..."
+          />
         </div>
         <div className="controls__item">
-          <label for="amount">Amount</label>
-          <input type="number" id="amount" min="0.01" step="0.01" />
+          <label>Amount</label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Enter price..."
+            min="0.01"
+            step="0.01"
+            onChange={changeAmountHandler}
+          />
         </div>
         <div className="controls__item">
-          <label for="date">Date</label>
-          <input id="date" type="date" min="2021-01-01" max="2022-12-31" />
+          <label>Date</label>
+          <input
+            name="date"
+            type="date"
+            min="2021-01-01"
+            max="2022-12-31"
+            onChange={changeDateHandler}
+          />
         </div>
       </div>
       <div className="actions">
